765ec2fa389a6b88e0e2f13a3d01123946b27b21
Konstantin <saigakov@yandex.ru>
2020-06-04 00:04:50 +0300

    themes WP

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+add_action('wp_enqueue_scripts', 'childhood_scripts');
+
+function childhood_scripts () {
+    wp_enqueue_style( 'childhood-style', get_stylesheet_uri() );
+    /* wp_enqueue_style( 'header-style', get_template_directory_uri() . '/assets/styles/main.min.css' );
+    wp_enqueue_style( 'animate-style', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.0/animate.compat.min.css'); */
+    wp_enqueue_script( 'childhood-scripts', get_template_directory_uri() . '/assets/js/main.min.js', array(), null, true );
+};
+
+add_theme_support( 'custom-logo' );
+
+
+
+
+?>
